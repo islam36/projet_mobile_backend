6aea29e3ff3c929bcc0c31324f5635fbff8b4d1b
islam36 <jm_boumendjel@esi.dz>
2023-04-26 12:51:11 +0100

    config mongoose

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-const app = require("./app");
 require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const DB_URL = process.env.DB_URL;
+
+mongoose.connect(DB_URL).then(() => {
+    console.log("connected successfully to the database!");
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.all("/", (req, res) => {
+    res.json({
+        message: "hello world"
+    });
+});
 
 const PORT = process.env.PORT || 8000;
 
